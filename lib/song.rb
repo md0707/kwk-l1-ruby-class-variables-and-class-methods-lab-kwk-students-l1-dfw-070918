4a6ba4ce2a77fcd8b5793df4ea43614ac245f483
md0707 <ppuggy1@gmail.com>
2018-07-16 14:32:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   attr_accessor :name, :artists, :genre
   @@count == 0
   @@artist_count == 0
+  @@genre_count == 0 
   @@artists = []
   @@genres == []
 
@@ -12,15 +13,20 @@ class Song
   @genre = genre
   @@count += 1
   @@artist_count += 1
+  @@genre_count += 1
   end
   
   def count 
     @count
   end
   
-  
+  def artist_count
+    @artist_count
+  end
     
-  
+  def genre_count
+    @genre_count
+  end
   
 end
 
